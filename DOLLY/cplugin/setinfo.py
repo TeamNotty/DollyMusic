4e9d7f1d66a420d11656742d943804361c9968c5
TeamNotty <nottyboy2026@gmail.com>
2026-01-03 02:03:16 +0530

    Update setinfo.py

--- DOLLY/cplugin/setinfo.py
+++ DOLLY/cplugin/setinfo.py
@@ -2,14 +2,14 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram import filters, Client
-from PROMUSIC import app
-from PROMUSIC.misc import SUDOERS
-from PROMUSIC.utils.decorators.language import language
+from DOLLY import app
+from DOLLY.misc import SUDOERS
+from DOLLY.utils.decorators.language import language
 
 from PROMUSIC.utils.database.clonedb import get_owner_id_from_db, get_cloned_support_chat, get_cloned_support_channel, check_bot_premium
 from config import SUPPORT_CHAT, OWNER_ID
 
-from PROMUSIC.utils.database import clonebotdb
+from DOLLY.utils.database import clonebotdb
 
 
 #set clone bot support channel
@@ -133,3 +133,4 @@ async def bot_info(client: Client, message: Message, _):
         f"➤ **Sᴜᴘᴘᴏʀᴛ Cʜᴀᴛ:** @{support}\n"
         f"➤ **Bᴏᴛ Sᴛᴀᴛᴜs:** {bot_status}"
     )
+
