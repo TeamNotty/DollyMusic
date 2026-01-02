dfbaf1c1b176ca833d07df6b92a749d7b2c600bb
TeamNotty <nottyboy2026@gmail.com>
2026-01-03 02:04:01 +0530

    Update settings.py

--- DOLLY/cplugin/settings.py
+++ DOLLY/cplugin/settings.py
@@ -8,8 +8,8 @@ from pyrogram.types import (
     Message,
 )
 
-from PROMUSIC import app
-from PROMUSIC.utils.database import (
+from DOLLY import app
+from DOLLY.utils.database import (
     add_nonadmin_chat,
     get_authuser,
     get_authuser_names,
@@ -25,15 +25,15 @@ from PROMUSIC.utils.database import (
     skip_off,
     skip_on,
 )
-from PROMUSIC.utils.decorators.admins import ActualAdminCB
-from PROMUSIC.utils.decorators.language import language, languageCB
-from PROMUSIC.utils.inline.settings import (
+from DOLLY.utils.decorators.admins import ActualAdminCB
+from DOLLY.utils.decorators.language import language, languageCB
+from DOLLY.utils.inline.settings import (
     auth_users_markup,
     playmode_users_markup,
     setting_markup,
     vote_mode_markup,
 )
-from PROMUSIC.utils.inline.start import private_panel
+from DOLLY.utils.inline.start import private_panel
 from config import BANNED_USERS, OWNER_ID
 
 
@@ -392,3 +392,4 @@ async def vote_change(client, CallbackQuery, _):
         )
     except MessageNotModified:
         return
+
