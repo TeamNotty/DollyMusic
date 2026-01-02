b4c518e0be09e96186a829f0b9ad31ac902b6da1
TeamNotty <nottyboy2026@gmail.com>
2026-01-03 02:09:04 +0530

    Update video.py

--- DOLLY/cplugin/video.py
+++ DOLLY/cplugin/video.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
 from time import time
-from PROMUSIC.utils.extraction import extract_user
+from DOLLY.utils.extraction import extract_user
 
 # Define a dictionary to track the last message timestamp for each user
 user_last_message_time = {}
@@ -205,3 +205,4 @@ async def ytmusic(client: Client, message: Message):
     for files in (sedlyf, file_stark):
         if files and os.path.exists(files):
             os.remove(files)
+
