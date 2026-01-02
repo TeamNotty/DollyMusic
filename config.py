24ab481bbcdd603e21f7eb39c641ce5643d309e1
TeamNotty <nottyboy2026@gmail.com>
2026-01-03 02:32:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 # ===============================================
 
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TeamNotty/DollyMusic")
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)  
 
 # ===============================================
