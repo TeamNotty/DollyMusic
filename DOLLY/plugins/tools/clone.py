6f4379833bac9e14baa383ac51eb6d64a876cb7d
TeamNotty <nottyboy2026@gmail.com>
2026-01-04 07:01:02 +0530

    Update clone.py

--- DOLLY/plugins/tools/clone.py
+++ DOLLY/plugins/tools/clone.py
@@ -101,8 +101,8 @@ async def clone_txt(client, message, _):
                 "name": bot.first_name,
                 "token": bot_token,
                 "username": bot.username,
-                "channel": "KomalMusicUpdate",
-                "support": "YARO_KA_ADDAA",
+                "channel": "NottySpace",
+                "support": "NottySupport",
                 "premium" : False,
                 "Date" : False,
             }
